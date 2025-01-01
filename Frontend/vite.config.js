6d2126e18c6f502e78f9aa55938f12821fe52d30
Sujan <rsujan140.in@gmail.com>
2025-01-01 19:18:50 +0545

    Frontend viteconfig js added customdomain setting

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: true, // Allow external access for debugging
+  },
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+  base: '/', // Ensures correct asset paths in production
 })
